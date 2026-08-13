869f71dc73f66eefd8af64072ce9974bfdb71ca4
Dheeraj <149280443+Jrdheeraj@users.noreply.github.com>
2026-08-13 21:52:30 +0530

    new files added

---
+++ atm.py
@@ -0,0 +1,9 @@
+class ATM:
+    def __init__(self, widthdraw):
+        self.balance = balance  
+    amount=int(input("Enter the amount to withdraw: "))
+    if amount < 500:
+        print("Minimum withdrawal amount is 500.")
+        
+    else: 
+        print("Please collect your cash.")
